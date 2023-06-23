db8d64af92f380f5f095ae968bfe7a0dd83ec5a0
Samyaksammy <132469256+Samyaksammy@users.noreply.github.com>
2023-06-23 14:24:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us14.api.mailchimp.com/3.0/lists/005714ade8";
+    const options =  {
+        method: "POST",
+        auth: "Samyak:358fe6b4ff525a377f406b4f72d68cb8-us14"
+    }
+
+    const mailchimprequest = https.request(url, options, function(response){
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        }else{
+          res.sendFile(__dirname + "/failure.html");  
+        }
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+
+    });
+    
+    mailchimprequest.write(jsonData);
+    mailchimprequest.end();
+
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Servers are working.");
+} )
+
+//	358fe6b4ff525a377f406b4f72d68cb8-us14
+//005714ade8.
